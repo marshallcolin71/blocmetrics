7b6b09e827dab3c73d6ca624044a307356dfd035
Colin <marshallcolin71@gmail.com>
2018-05-24 15:11:44 -0400

    implemented Devise and user sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,18 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :address              => ENV['EMAIL_SMTP'],
+  :port                 => 587,
+  :user_name            => ENV['EMAIL_USERNAME'],
+  :password             => ENV['EMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -36,6 +48,5 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+
 end
